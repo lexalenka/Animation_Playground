15b9c1639c9bc7b228638bbbe69ce3bce6b7787f
v622721 <alexandra.haracz@one.verizon.com>
2017-01-18 13:20:23 -0800

    Cleaning up spacing

--- main/java/com/lexalenka/animationplayground/MainActivity.java
+++ main/java/com/lexalenka/animationplayground/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
+        // Pass any configuration change to the drawer toggles
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
